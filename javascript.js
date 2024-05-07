3c1e8f88227af0f1e554440c8e10e5dd27843cdb
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-07 20:58:42 +0200

    files

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function showInfo() {
+    var infoText = document.getElementById("info-text");
+    if (infoText.style.display === "none") {
+        infoText.style.display = "block";
+    } else {
+        infoText.style.display = "none";
+    }
+}
