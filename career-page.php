81d8e64486f4275438582f3602e5a908193e8b58
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-08 11:13:55 +0200

    position list test

--- career-page.php
+++ career-page.php
@@ -8,9 +8,37 @@ Template Name: Career
 
 <main>
 
-    <?php while(have_posts()): the_post(); ?>
-        <?php the_content(); ?>
-    <?php endwhile; ?>
+    <h2>Open Positions</h2>
+
+    <?php
+    
+    $related_positions = get_field('related_job_positions');
+
+    if ($related_positions) :
+        ?>
+        <ul class="position-list">
+            <?php foreach ($related_positions as $post) : setup_postdata($post); ?>
+                <li class="position-item">
+                    <h3><?php the_title(); ?></h3>
+                    <p>
+                        <?php
+                        $job_location = get_field('job_location');
+                        $job_type = get_field('job_type');
+                        echo $job_location . ' | ' . $job_type;
+                        ?>
+                    </p>
+                    <a href="<?php the_permalink(); ?>" class="learn-more">Learn more</a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+
+        <?php
+        // Restore original post data
+        wp_reset_postdata();
+    else :
+        ?>
+        <p>No open positions currently available.</p>
+    <?php endif; ?>
 
 </main>
 
