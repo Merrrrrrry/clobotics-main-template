4f65b593c7d6ecd051416ad9596830b38f4a3572
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-08 10:26:30 +0200

    adding code to each page

--- about-us-page.php
+++ about-us-page.php
@@ -4,8 +4,15 @@ Template Name: About us
 */
 ?>
 
-<?php get_header() ?>
-    
-    <?php while(have_posts()): the_post() ?>
-  
-<body>
+<?php get_header(); ?>
+
+<main>
+
+    <?php while(have_posts()): the_post(); ?>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+
+</main>
+
+<?php get_footer(); ?>
+
--- career-page.php
+++ career-page.php
@@ -4,8 +4,14 @@ Template Name: Career
 */
 ?>
 
-<?php get_header() ?>
-    
-    <?php while(have_posts()): the_post() ?>
-  
-<body>
+<?php get_header(); ?>
+
+<main>
+
+    <?php while(have_posts()): the_post(); ?>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+
+</main>
+
+<?php get_footer(); ?>
