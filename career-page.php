044580d3ebdf04ada1914cb74fa15126720189fb
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-08 12:40:32 +0200

    search bar again

--- career-page.php
+++ career-page.php
@@ -12,17 +12,26 @@ Template Name: Career
 
     <!-- Search Bar -->
     <form method="get" action="<?php echo esc_url(home_url('/')); ?>">
-        <input type="text" name="s" placeholder="Search for positions">
+        <input type="text" name="s" placeholder="Search for positions" value="<?php echo get_search_query(); ?>">
         <button type="submit">Search</button>
     </form>
 
     <?php
-    
-    $related_positions = get_field('related_job_positions');
+    // Get search query
+    $search_query = sanitize_text_field($_GET['s']);
 
-    if ($related_positions) : ?>
+    // Perform search query
+    $args = array(
+        'post_type' => 'open_positions', // Replace with your custom post type
+        'posts_per_page' => -1, // Display all positions
+        's' => $search_query, // Search query
+    );
+
+    $search_results = new WP_Query($args);
+
+    if ($search_results->have_posts()) : ?>
         <ul class="position-list">
-            <?php foreach ($related_positions as $post) : setup_postdata($post); ?>
+            <?php while ($search_results->have_posts()) : $search_results->the_post(); ?>
                 <li class="position-item">
                     <h3><?php the_title(); ?></h3>
                     <p>
@@ -34,16 +43,18 @@ Template Name: Career
                     </p>
                     <a href="<?php the_permalink(); ?>" class="learn-more">Learn more</a>
                 </li>
-            <?php endforeach; ?>
+            <?php endwhile; ?>
         </ul>
         <?php
         // Restore original post data
         wp_reset_postdata();
-    else :
-        ?>
+    elseif ($search_query) : ?>
+        <p>No positions found for '<?php echo $search_query; ?>'</p>
+    <?php else : ?>
         <p>No open positions currently available.</p>
     <?php endif; ?>
 
 </main>
 
 <?php get_footer(); ?>
+
