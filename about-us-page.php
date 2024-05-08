b60df1537d59e83116eb5da89b20aa553700189d
Merrrrrrry <grs.merry@gmail.com>
2024-05-08 23:24:25 +0200

    Update about-us-page.php

--- about-us-page.php
+++ about-us-page.php
@@ -54,6 +54,20 @@ Template Name: About us
 </div>
 
 
+<!-- Article Section -->
+<!-- One company Two directions -->
+
+<div class="article_secondary_main one_company_two_directions">
+
+    <h3 class="article_title article_title_one_company_two_directions"><?php the_field('article_title_one_company_two_directions'); ?></h3>
+    <p class="wind_part_text"><?php the_field('wind_part_text'); ?></p>
+    <p class="retail_part_text"><?php the_field('retail_part_text'); ?></p>
+        <a href="<?php echo get_permalink('  ............  ') ?>" class="button_blue">Video presentation</a>
+        <img class="about-us-page one_company_two_directions_collage_img " src="  ............  " alt=""> 
+        
+</div>
+
+
 
 
 
