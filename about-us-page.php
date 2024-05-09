592e455dfb68e1565fcdd3d5cbc4794667668479
Merrrrrrry <grs.merry@gmail.com>
2024-05-09 13:13:06 +0200

    last changes before domain merge

--- about-us-page.php
+++ about-us-page.php
@@ -11,7 +11,7 @@ Template Name: About us
     <?php while(have_posts()): the_post(); ?>
         <?php the_content(); ?>
 
-<body class="maria-who-we-are">
+<body>
 
 <!-- Hero Section WITHOUT navbar -->
 
