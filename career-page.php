60644ff97f73e4b3fa0210ebfaeefbfbba97163d
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-09 15:28:22 +0200

    search bar test

--- career-page.php
+++ career-page.php
@@ -10,35 +10,54 @@ Template Name: Career
 
     <h2>Open Positions</h2>
 
-    <?php
+     <!-- Search bar -->
+
+    <div class="search-bar">
+        <form method="get" action="">
+            <input type="text" name="search_query" id="search-input" placeholder="Search...">
+            <button type="submit">Search</button>
+        </form>
+    </div>
+
     
-    $related_positions = get_field('related_job_positions');
 
-    if ($related_positions) :
-        ?>
-        <ul class="position-list">
-            <?php foreach ($related_positions as $post) : setup_postdata($post); ?>
-                <li class="position-item">
-                    <h3><?php the_title(); ?></h3>
-                    <p>
-                        <?php
-                        $job_location = get_field('job_location');
-                        $job_type = get_field('job_type');
-                        echo $job_location . ' | ' . $job_type;
-                        ?>
-                    </p>
-                    <a href="<?php the_permalink(); ?>" class="learn-more">Learn more</a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+            <!-- Job Positions -->
+<?php
+// Retrieve search query from URL parameter
+$search_query = isset($_GET['search_query']) ? sanitize_text_field($_GET['search_query']) : '';
 
-        <?php
-       
-        wp_reset_postdata();
-    else :
-        ?>
-        <p>No open positions currently available.</p>
-    <?php endif; ?>
+// Query job positions based on search query
+$args = array(
+    'post_type' => 'open-position',
+    'posts_per_page' => -1, 
+    's' => $search_query, // Search query
+);
+
+$related_positions = new WP_Query($args);
+
+if ($related_positions->have_posts()) :
+    ?>
+    <ul class="position-list">
+        <?php while ($related_positions->have_posts()) : $related_positions->the_post(); ?>
+            <li class="position-item">
+                <h3><?php the_title(); ?></h3>
+                <p>
+                    <?php
+                    $job_location = get_field('job_location');
+                    $job_type = get_field('job_type');
+                    echo $job_location . ' | ' . $job_type;
+                    ?>
+                </p>
+                <a href="<?php the_permalink(); ?>" class="learn-more">Learn more</a>
+            </li>
+        <?php endwhile; ?>
+    </ul>
+    <?php
+    wp_reset_postdata();
+else :
+    ?>
+    <p>No open positions found for "<?php echo $search_query; ?>"</p>
+<?php endif; ?>
 
 </main>
 
